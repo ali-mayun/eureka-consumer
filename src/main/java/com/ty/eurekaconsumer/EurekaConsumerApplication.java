a5d574cd2ae5b6c266dea0a794be3891bb92bb24
ali-mayun <46866865+ali-mayun@users.noreply.github.com>
2019-05-09 22:28:22 +0800

    20190509添加Hystrix
    基于RestTemplate+Ribbon，添加Hystrix

--- src/main/java/com/ty/eurekaconsumer/EurekaConsumerApplication.java
+++ src/main/java/com/ty/eurekaconsumer/EurekaConsumerApplication.java
@@ -3,9 +3,12 @@ package com.ty.eurekaconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableEurekaClient
 @SpringBootApplication
+//使用这注解开启hystrix的功能
+@EnableHystrix
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ty/eurekaconsumer/service/RibbonService.java
+++ src/main/java/com/ty/eurekaconsumer/service/RibbonService.java
@@ -1,5 +1,6 @@
 package com.ty.eurekaconsumer.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,14 @@ public class RibbonService {
     @Autowired
     private RestTemplate restTemplate;
 
+    //使用@HystrixCommand注解启用熔断器的功能，当服务熔断后，将不调用远程服务，直接调用本地的handleError方法
+    @HystrixCommand(fallbackMethod = "handleError")
     public String hiService(String name) {
-        return restTemplate.getForObject("http://eureka-provider/firstCall?name="+name, String.class);
+        return restTemplate.getForObject("http://eureka-provider/firstCall?name=" + name, String.class);
+    }
+
+    public String handleError(String name) {
+        return name + "在访问远程服务时，被熔断了，调用本地的方法";
     }
 }
-
 
